18b468c5cf07e64d18a8d942a9db68d1dd6fb100
mitchelllewisnelson <mitchelllewisnelson@gmail.com>
2022-03-20 21:13:30 -0600

    Add files via upload

--- vulguard.js
+++ vulguard.js
@@ -1,10 +1,7 @@
+//Turn on subtitles to filter
 
-
-
-//Turn on subtitles
-//document.getElementsByClassName("ytp-subtitles-button ytp-button")[0].click();
-console.log("STARTING!!!!!!!")
-
+console.log("Starting Vulguard")
+var video=document.querySelectorAll("video, audio")[0];
 var subtitle_class="";
 
 url_parts=location.hostname.split(".")
@@ -23,25 +20,21 @@ else if (domain == "youtube"){
 	subtitle_class="ytp-caption-window-container"
 }
 
-
 var time_without_subs=0;
 
-//sub_section=document.getElementsByClassName("captions-text")[0];
-																								var blacklist=["politic", "god","damn","shit","bastard","jesus"," lord ","christ","ass","hell","dick","vagina","idiot","moron","trash","psycho"];
+var blacklist=																										[/politic/,/(my|oh|to) god/,/god.? no/,/damn/,/shit/,/bastard/,/jesus/,/lord/,/christ/,/\bass\b/,/asshole/,/asshat/,/\bhell\b/,/dick/,/vagina/,/penis/,/sex/,/whore/,/slut/,/cunt/,/idiot/,/moron/]
 
+console.log(blacklist)
 var styleSheet = document.createElement("style")
 styleSheet.type = "text/css"
 styleSheet.innerText = "."+subtitle_class+"{margin-left:2000%};"
 document.head.appendChild(styleSheet)
 
-
-
-
 var checker_interval = setInterval(check4subs,2000);
 var filter_interval='';
 
 function check4subs() {
-	console.log("checking");
+	console.log("searching for subtitles");
 	if (document.getElementsByClassName(subtitle_class)[0].innerText.length>0){
 		console.log("found subtitles");
 		clearInterval(checker_interval);
@@ -49,10 +42,11 @@ function check4subs() {
 	}
 }
 
+
 function filter(){  
 	try{
-		if (document.getElementsByClassName("subtitle_class")[0].innerText.length<1){
-			throw "no subtitles";
+		if (document.getElementsByClassName(subtitle_class)[0].innerText.length<1){
+			throw "no subtitles found";
 		}
 		//console.log("hiding subs");
 		time_without_subs=0;
@@ -61,23 +55,23 @@ function filter(){
 		time_without_subs+=1;
 		console.log(time_without_subs);
 		if (time_without_subs>3000){
+			time_without_subs=0;
 			clearInterval(filter_interval);
 			checker_interval=setInterval(check4subs,2000);
 		}
 	}
 	
-	//Get the words
+	//Get subtitle text
 	var subtitle_text="";
 	try{
 		subtitle_text=document.getElementsByClassName(subtitle_class)[0].innerText;
 	}
 	catch(err){}
 	
-	
-	//Check if a blacklist word appears, and if so, mute.
+	//Check if a blacklist word is present in subtitle text, and if so, set do_mute to true
 	var do_mute=false;
-	for (var i=0 ; i<blacklist.length ; i++){
-		if(subtitle_text.toLowerCase().indexOf(blacklist[i]) >= 0){
+	for (var i=0 ; i<blacklist.length ; i++){		
+		if (subtitle_text.toLowerCase().match(blacklist[i])){
 			console.log("blacklist word: "+blacklist[i]);
 			do_mute=true;
 			break;
@@ -85,9 +79,7 @@ function filter(){
 	}
 
 	//console.log(do_mute);
-	
-	var video=document.querySelectorAll("video, audio")[0];
-	
+
 	if (do_mute){
 		if (video.muted==false){
 			video.muted=true;
